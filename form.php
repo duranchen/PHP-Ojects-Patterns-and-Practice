2a7098eef66d42975f1a20b3539c2b315fa350c6
duranchen <duran.chen@gmail.com>
2014-03-27 08:43:42 +0800

    add a new form.php for testing decimals field submit.

---
+++ form.php
@@ -0,0 +1,20 @@
+<?php
+$input = $_POST['input_name'];
+if (isset($input)) {
+    var_dump($input);
+}
+
+?>
+
+
+<html>
+<head></head>
+<body>
+	<form action="" method="post">
+
+		<input name="input_name" type="text" id="input_id">
+
+	</form>
+
+</body>
+</html>
